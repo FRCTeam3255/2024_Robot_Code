6567a56466716dbd2cc2f284051c9762a5004f6c
Alenguye582 <112772895+Alenguye582@users.noreply.github.com>
2024-01-15 13:55:37 -0800

    created new empty subsystems

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap.mapClimber;
+
+public class Climber extends SubsystemBase {
+
+  public Climber() {
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
