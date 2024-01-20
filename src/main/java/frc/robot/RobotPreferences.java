c81d5a2d303908aadbc46212d9d15fcfcfb8a429
Alena <112772895+Alenguye582@users.noreply.github.com>
2024-01-20 10:44:20 -0800

    the intake can intake (#31)
    hopefully

--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -130,4 +130,9 @@ public class RobotPreferences {
         0);
 
   }
+
+  public static final class prefIntake {
+    public static final SN_DoublePreference intakeSpeed = new SN_DoublePreference("intakeSpeed", 0.5);
+    public static final SN_DoublePreference intakeCenteringSpeed = new SN_DoublePreference("intakeSpeed", 0.5);
+  }
 }
