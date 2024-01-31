29f306b99b3e4b44294ee83e7a7d08bf40d32c5c
dunep <144501214+dunep@users.noreply.github.com>
2024-01-30 19:23:33 -0800

    Add transfer (#64)
    * added transfers subsystem
    
    * added functionality to transfer
    
    ---------
    
    Co-authored-by: BrodyKarr <145169276+BrodyKarr@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/Transfer.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap.mapTransfer;
+import frc.robot.RobotPreferences.transferPref;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix6.controls.NeutralOut;
+
+public class Transfer extends SubsystemBase {
+  /** Creates a new Transfer. */
+  public void configure() {
+  }
+
+  TalonSRX transferMotor;
+
+  public Transfer() {
+    transferMotor = new TalonSRX(mapTransfer.TRANSFER_MOTOR_CAN);
+  }
+
+  public void setTranferMotorSpeed(double motorSpeed) {
+    transferMotor.set(ControlMode.PercentOutput, motorSpeed);
+  }
+
+  public void setNeutralOutput() {
+    transferMotor.neutralOutput();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
