2e47467636d9bcd0b76c6e2b165ff7e0adf794e2
ACat701 <90939494+ACat701@users.noreply.github.com>
2024-02-03 00:58:44 -0800

    Log Locked Rotation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import monologue.Monologue;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -26,7 +27,11 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     Monologue.updateAll();
     CommandScheduler.getInstance().run();
+
+    // Logging to SmartDashboard
     RobotContainer.logPDHValues();
+    SmartDashboard.putBoolean("Is Practice Bot", RobotContainer.isPracticeBot());
+    SmartDashboard.putString("Current Locked Location", RobotContainer.getLockedLocation().toString());
   }
 
   @Override
