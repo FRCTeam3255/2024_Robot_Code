b0738858b083ecce5345c484c7ecc0f6fab104f2
Alenguye582 <112772895+Alenguye582@users.noreply.github.com>
2024-02-02 17:54:35 -0800

    added LED functionality
    mapped amplify and co op to buttons

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -4,19 +4,47 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.led.Animation;
+import com.ctre.phoenix.led.CANdle;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.constLEDs;
+import frc.robot.RobotMap.mapLEDs;
 
 // CANdle CANdle;
 
 public class LEDs extends SubsystemBase {
+  CANdle CANdle;
+
   /** Creates a new LEDs. */
   public LEDs() {
-    // CANdle = new CANdle(mapLEDs.CANDLE_CAN);
+    CANdle = new CANdle(mapLEDs.LEDs_CANdle_ID);
     configure();
   }
 
   public void configure() {
+    CANdle.configFactoryDefault();
+    CANdle.configBrightnessScalar(constLEDs.LED_BRIGHTNESS);
+  }
 
+  public void setLEDs(int[] rgb) {
+    CANdle.setLEDs(rgb[0], rgb[1], rgb[2]);
+  }
+
+  /**
+   * Types of Animations available:
+   * ColorFlowAnimation, FireAnimation, LarsonAnimation, RainbowAnimation,
+   * RgbFadeAnimation, SingleFadeAnimation, StrobeAnimation, TwinkleAnimation,
+   * TwinkleOffAnimation
+   * 
+   * @param animation Type of Animation desired
+   */
+  public void setLEDsToAnimation(Animation animation) {
+    CANdle.animate(animation, 0);
+  }
+
+  public void clearAnimation() {
+    CANdle.clearAnimation(0);
   }
 
   @Override
