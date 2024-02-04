6a2a484f8c252cd4e6eac8d3c7846a02b87b973d
ACat701 <90939494+ACat701@users.noreply.github.com>
2024-02-04 02:29:09 -0800

    Create LockPitch.java

---
+++ src/main/java/frc/robot/commands/LockPitch.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.FieldConstants;
+import frc.robot.RobotContainer;
+import frc.robot.Constants.constTurret.LockedLocation;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Shooter;
+
+public class LockPitch extends Command {
+  Shooter subShooter;
+  Drivetrain subDrivetrain;
+
+  LockedLocation lockedLocation = LockedLocation.NONE;
+
+  Rotation2d desiredAngle = new Rotation2d();
+
+  Optional<Alliance> alliance = DriverStation.getAlliance();
+
+  Pose3d[] fieldPoses;
+  Pose3d speakerPose;
+  Pose3d ampPose;
+  Pose2d robotPose = new Pose2d();
+
+  public LockPitch(Shooter Shooter, Drivetrain subDrivetrain) {
+    this.subShooter = Shooter;
+    this.subDrivetrain = subDrivetrain;
+
+    // TODO: figure out if this implementation will cause problems with shooting (im
+    // pretty sure it will, we need to think about this)
+    addRequirements(subShooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    desiredAngle = Rotation2d.fromDegrees(subShooter.getPitchAngle());
+
+    fieldPoses = FieldConstants.GET_FIELD_POSITIONS();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    robotPose = subDrivetrain.getPose();
+
+    Optional<Rotation2d> calculatedAngle = subShooter.getDesiredAngleToLock(robotPose, fieldPoses,
+        RobotContainer.getLockedLocation());
+
+    if (calculatedAngle.isPresent()) {
+      desiredAngle = calculatedAngle.get();
+    }
+    // Otherwise, try to go to the last calculated angle
+
+    if (subShooter.isAnglePossible(desiredAngle.getDegrees())) {
+      subShooter.setPitchAngle(desiredAngle.getDegrees());
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
