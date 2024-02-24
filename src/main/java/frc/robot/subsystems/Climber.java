3818cd9e065ca22177ec20bdba6b71f9f3c6564d
Vaddin <dunpulve757@euhsd.org>
2024-02-23 20:29:22 -0800

    voltage import begone

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -9,7 +9,6 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.Constants.constClimber;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.units.Voltage;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap.mapClimber;
