e60b556c53c26a352dddf3f1c80ee816e5ce8810
ACat701 <90939494+ACat701@users.noreply.github.com>
2024-02-25 16:41:58 -0800

    Add and update comments

--- src/main/java/frc/robot/subsystems/Transfer.java
+++ src/main/java/frc/robot/subsystems/Transfer.java
@@ -13,13 +13,11 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.controls.NeutralOut;
-import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Transfer extends SubsystemBase {
   TalonSRX feederMotor;
   TalonFX transferMotor;
-  VelocityVoltage velocityRequest;
   CurrentLimitsConfigs transferCurrentLimitConfigs;
 
   private boolean hasGamePiece;
@@ -30,7 +28,6 @@ public class Transfer extends SubsystemBase {
 
     transferCurrentLimitConfigs = new CurrentLimitsConfigs();
 
-    velocityRequest = new VelocityVoltage(0).withSlot(0);
     configure();
   }
 
@@ -46,8 +43,8 @@ public class Transfer extends SubsystemBase {
 
   /**
    * Calculates and sets the value of hasGamePiece based off of Transfer & Feeder
-   * Current & Velocity. If we already have a game piece, this return true without
-   * recalculating.
+   * current & velocity. If we already have a game piece, this will return true
+   * without recalculating.
    * 
    * @return If we have a game piece.
    */
@@ -68,31 +65,64 @@ public class Transfer extends SubsystemBase {
     return hasGamePiece;
   }
 
+  /**
+   * Set if we have a game piece without recalculating it based off of current
+   * 
+   * @param isCollected If we currently have a game piece.
+   */
   public void setGamePieceCollected(boolean isCollected) {
     hasGamePiece = isCollected;
   }
 
+  /**
+   * Sets the current speed of the Feeder motor
+   * 
+   * @param feederSpeed The speed to set the Feeder motor to. <b> Units: </b>
+   *                    Percent Output (-1.0 -> 1.0)
+   */
   public void setFeederMotorSpeed(double feederSpeed) {
     feederMotor.set(ControlMode.PercentOutput, feederSpeed);
 
   }
 
+  /**
+   * Sets the current speed of the Transfer motor
+   * 
+   * @param transferSpeed The speed to set the Transfer motor to. <b> Units: </b>
+   *                      Percent Output (-1.0 -> 1.0)
+   */
   public void setTransferMotorSpeed(double transferSpeed) {
     transferMotor.set(transferSpeed);
   }
 
+  /**
+   * Sets the Feeder motor to neutral output.
+   */
   public void setFeederNeutralOutput() {
     feederMotor.neutralOutput();
   }
 
+  /**
+   * Sets the Transfer motor to neutral output.
+   */
   public void setTransferNeutralOutput() {
     transferMotor.setControl(new NeutralOut());
   }
 
+  /**
+   * Get the current percent-output for the Transfer motor
+   * 
+   * @return The percent output (-1.0 - > 1.0)
+   */
   public double getTransferMotorPercentOutput() {
     return transferMotor.get();
   }
 
+  /**
+   * Get the current percent-output for the Feeder motor
+   * 
+   * @return The percent output (-1.0 - > 1.0)
+   */
   public double getFeederMotorPercentOutput() {
     return feederMotor.getMotorOutputPercent();
   }
