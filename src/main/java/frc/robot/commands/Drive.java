eb953c62ce6fe837ea53f733a5bd29149a959833
ACat701 <90939494+ACat701@users.noreply.github.com>
2024-02-27 12:24:36 -0800

    Add triggers to Drive.java

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -6,11 +6,13 @@ package frc.robot.commands;
 
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.FieldConstants;
 import frc.robot.Constants.constDrivetrain;
 import frc.robot.RobotPreferences.prefDrivetrain;
 import frc.robot.subsystems.Drivetrain;
@@ -19,10 +21,12 @@ public class Drive extends Command {
   Drivetrain subDrivetrain;
   DoubleSupplier xAxis, yAxis, rotationAxis;
   boolean isPracticeBot, isOpenLoop;
-  Trigger slowMode, northTrigger, eastTrigger, southTrigger, westTrigger;
+  Trigger slowMode, northTrigger, eastTrigger, southTrigger, westTrigger, sourceTrigger, trapTrigger;
 
   double driveSpeed, xVelocity, yVelocity, rVelocity, slowMultiplier;
   Translation2d translationVelocity;
+  Rotation2d sourceAngle, leftStageAngle, rightStageAngle, centerStageAngle;
+  Pose3d[] fieldPositions;
 
   public Drive(
       Drivetrain subDrivetrain,
@@ -34,6 +38,8 @@ public class Drive extends Command {
       Trigger eastTrigger,
       Trigger southTrigger,
       Trigger westTrigger,
+      Trigger sourceTrigger,
+      Trigger trapTrigger,
       boolean isPracticeBot) {
 
     this.subDrivetrain = subDrivetrain;
@@ -47,6 +53,8 @@ public class Drive extends Command {
     this.eastTrigger = eastTrigger;
     this.southTrigger = southTrigger;
     this.westTrigger = westTrigger;
+    this.sourceTrigger = sourceTrigger;
+    this.trapTrigger = trapTrigger;
 
     isOpenLoop = false;
 
@@ -56,6 +64,12 @@ public class Drive extends Command {
   @Override
   public void initialize() {
     driveSpeed = (isPracticeBot) ? constDrivetrain.pracBot.DRIVE_SPEED : constDrivetrain.DRIVE_SPEED;
+    fieldPositions = FieldConstants.GET_FIELD_POSITIONS();
+
+    sourceAngle = fieldPositions[2].getRotation().toRotation2d();
+    leftStageAngle = fieldPositions[3].getRotation().toRotation2d();
+    centerStageAngle = fieldPositions[4].getRotation().toRotation2d();
+    rightStageAngle = fieldPositions[5].getRotation().toRotation2d();
   }
 
   @Override
@@ -80,6 +94,18 @@ public class Drive extends Command {
         rVelocity = subDrivetrain.getVelocityToSnap(Rotation2d.fromDegrees(0));
       } else if (westTrigger.getAsBoolean()) {
         rVelocity = subDrivetrain.getVelocityToSnap(Rotation2d.fromDegrees(-90));
+      } else if (sourceTrigger.getAsBoolean()) {
+        rVelocity = subDrivetrain.getVelocityToSnap(sourceAngle);
+      } else if (trapTrigger.getAsBoolean()) {
+        // TRAP LOGIC!??!?!?!? no way
+        String debugString = "Closest Chain: NONE";
+        // I wrote the math for this
+        // But i have no clue
+        // how to code it
+        // send help
+        // https://www.desmos.com/calculator/l1ntqvcuv3
+
+        System.out.println(debugString);
       }
     }
 
