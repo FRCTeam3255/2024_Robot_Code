d9166ee04ed7db2a0e41ebf444203aaed3cbb935
Alena <112772895+Alenguye582@users.noreply.github.com>
2024-03-14 07:26:09 -0700

    missing prefIntake import for some reason

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -10,6 +10,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap.mapIntake;
+import frc.robot.RobotPreferences.prefIntake;
 
 public class Intake extends SubsystemBase {
   TalonFX rollerMotor;
